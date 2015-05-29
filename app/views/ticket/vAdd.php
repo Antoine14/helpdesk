78a0f7842420a7639aa1c66c2760d5037a4faddc
malo53 <21407415@etu.unicaen.fr>
2015-05-29 16:13:11 +0200

    Revert "Tickets et vAdd"
    This reverts commit 0e80ce2f12b316123247b82216982011714e5319.

--- app/views/ticket/vAdd.php
+++ app/views/ticket/vAdd.php
@@ -4,11 +4,11 @@
 <div class="alert alert-info">Utilisateur : <?php echo $ticket->toString()?></div>
 <div class="form-group">
 	<input type="hidden" name="id" value="<?php echo $ticket->getId()?>">
-	<input type="text" name="titre" value="<?php echo $ticket->getMail()?>" placeholder="Entrez le titre" class="form-control">
+	<input type="mail" name="titre" value="<?php echo $user->getMail()?>" placeholder="Entrez l'adresse email" class="form-control">
 </div>
 <div class="form-group">
 	<input type="submit" value="Valider" class="btn btn-default">
-	<a class="btn btn-default" href="<?php echo $config["siteUrl"]?>tickets">Annuler</a>
+	<a class="btn btn-default" href="<?php echo $config["siteUrl"]?>users">Annuler</a>
 </div>
 </fieldset>
 </form>
